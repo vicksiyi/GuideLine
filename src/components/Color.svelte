a96cb70cf9ec26cf99977cee9dc61f9090113bfe
ZeroIce <tingwei.chen@js.design>
2022-07-07 08:59:17 +0800

    feat:添加颜色选择器组件

---
+++ src/components/Color.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    import { onMount } from 'svelte';
+    const dispatch = createEventDispatcher();
+    export let basedColor = '#CCCCCC';
+    export let width = 16;
+    export let height = 16;
+
+    // 显示颜色选择器
+    function showColorPalate(){
+        let selectColorInput = document.getElementById("select-color-input");
+        selectColorInput && selectColorInput.click();
+        selectColorInput && selectColorInput.addEventListener("input", (event) => {
+            basedColor = event.target.value;
+            dispatch("colorChange", { basedColor });
+        });
+    }
+    
+    onMount(()=>{
+        // 监听点击事件
+        let selectNode = document.getElementById("colors");
+        selectNode && selectNode.addEventListener('click', showColorPalate);
+    })
+</script>
+
+<div class="color-select">
+  <div
+    id="colors"
+    class="based-color"
+    style="background-color: {basedColor};width: {width}px;height: {height}px"
+  />
+  <input type="color" id="select-color-input" style="visibility: hidden;" />
+</div>
+
+<style>
+</style>
